a400fb7b319a777de8368a31560dda0fcfec1ddf
Billy Ceskavich <bceskavich@gmail.com>
2015-09-30 13:35:12 -0400

    Updated base files

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className='title'>NEXIS-X React Todo App</h1>
+      </div>
+    );
+  }
+}
+
+React.render(<TodoApp />, document.getElementById('app'));
